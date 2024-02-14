67cecabaa11cdebf31e28372ebdad9190a49ec65
Piyush dana <piyushdana11@gmail.com>
2024-02-14 23:14:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home.js"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Projects from './pages/Projects.js'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    // <div>
+    //   <Home/>
+    // </div>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        {/* Add more routes for other pages if needed */}
+      </Routes>
+
+      
+    </Router>
   );
+  
 }
 
 export default App;
+
+
---
+++ src/pages/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Project.css'
+
+const Projects = () => {
+  return (
+    <div>
+        hello
+    </div>
+  )
+}
+
+export default Projects
